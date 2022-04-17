65900f73488d3fb06cfae002239cf31369c06feb
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-04-16 22:47:50 -0300

    refactor
    change all models to functions instead of class

--- models/blogposts.js
+++ models/blogposts.js
@@ -1,27 +1,10 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class BlogPosts extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  BlogPosts.init({
+  const BlogPosts = sequelize.define('BlogPosts', {
     title: DataTypes.STRING,
     content: DataTypes.STRING,
     userId: DataTypes.INTEGER,
     published: DataTypes.DATE,
-    updated: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'BlogPosts',
+    updated: DataTypes.DATE,
   });
   return BlogPosts;
 };
--- models/postscategories.js
+++ models/postscategories.js
@@ -1,24 +1,7 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class PostsCategories extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  PostsCategories.init({
+  const PostsCategories = sequelize.define('PostCategories', {
     categoryId: DataTypes.INTEGER,
-    postId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'PostsCategories',
+    postId: DataTypes.INTEGER,
   });
   return PostsCategories;
 };
