5a77129fcbe15881185ed96acc02c520792ca41f
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-04-19 17:20:13 -0300

    refactor
    change the way to call functions, and implement a handleError.

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 require('dotenv').config();
 const express = require('express');
+const userRoute = require('./routes/userRoute');
+const postRoute = require('./routes/postRoute');
+const categoriesRoute = require('./routes/categoriesRoute');
+const loginRoute = require('./routes/loginRoute');
+const handleError = require('./middleware/handleErrors');
 
 const app = express();
 app.use(express.json());
 
-app.use('/user', require('./routes/userRoute'));
-app.use('/post', require('./routes/postRoute'));
-app.use('/categories', require('./routes/categoriesRoute'));
-
+app.use('/user', userRoute);
+app.use('/post', postRoute);
+app.use('/categories', categoriesRoute);
+app.use('/login', loginRoute);
+app.use(handleError);
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
 // não remova esse endpoint, e para o avaliador funcionar
