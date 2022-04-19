3e100415ada4d808af3fa0074597f3d5c382d075
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-04-19 18:17:58 -0300

    feat
    implement a index with all functions to provide a better way to call.

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+const createUser = require('./createUser');
+const listUsers = require('./listUsers');
+const login = require('./login');
+
+module.exports = {
+  createUser,
+  listUsers,
+  login,
+};
