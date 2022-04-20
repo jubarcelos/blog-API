0390bf663b1288ff6f2f7a02af88683f59febaf5
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-04-19 21:13:19 -0300

    remove logs

--- middleware/validInputs.js
+++ middleware/validInputs.js
@@ -1,6 +1,5 @@
 const validInputs = (schema) => (req, res, next) => {
   const { error } = schema.validate(req.body);
-  console.log({ error });
   if (error) throw error;
   next();
 };
--- services/login.js
+++ services/login.js
@@ -2,7 +2,6 @@ const { User } = require('../models');
 
 const login = async ({ email, password }) => {
   const user = await User.findOne({ where: { email, password } });
-  console.log(user);
   if (!user) {
     return { status: 400, message: 'Invalid fields' };
   }
