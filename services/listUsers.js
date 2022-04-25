f8d32143627162cfa20d0a528086160735655a06
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-04-25 15:04:35 -0300

    Fix names
    Plural is better because list all users.

--- services/listUsers.js
+++ services/listUsers.js
@@ -1,8 +1,8 @@
 const { User } = require('../models');
 
-const listUser = async () => {
+const listUsers = async () => {
   const listedUsers = await User.findAll();
   return listedUsers;
 };
 
-module.exports = listUser;
+module.exports = listUsers;
