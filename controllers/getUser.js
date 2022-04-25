570a1908f4a1eb6a93328989a8b4341658913177
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-04-25 10:00:10 -0300

    Feat getUser function
    created and implemented getUser controller function.

---
+++ controllers/getUser.js
@@ -0,0 +1,13 @@
+const Service = require('../services');
+
+const getUserController = async (req, res, next) => {
+  try {
+    const gotUser = await Service.getUser(req.params.id);
+    if (gotUser.message) return res.status(gotUser.status).json({ message: gotUser.message });
+    return res.status(200).json(gotUser);
+  } catch (error) { 
+    next(error);
+  }
+};
+
+module.exports = getUserController;
