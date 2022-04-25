a2eb0a8cde20993f9a62b67e298b2d5e59283dfc
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-04-25 15:00:08 -0300

    Fix get user
     delete password from exibition.

--- controllers/getUser.js
+++ controllers/getUser.js
@@ -4,6 +4,7 @@ const getUserController = async (req, res, next) => {
   try {
     const gotUser = await Service.getUser(req.params.id);
     if (gotUser.message) return res.status(gotUser.status).json({ message: gotUser.message });
+    delete gotUser.dataValues.password;
     return res.status(200).json(gotUser);
   } catch (error) { 
     next(error);
