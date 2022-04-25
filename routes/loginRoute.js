a88313918eeffd39d29bf882db49c4f88442971b
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-04-25 18:31:43 -0300

    clean comments

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -2,17 +2,9 @@ const loginRoutes = require('express').Router();
 const validInputs = require('../middleware/validInputs');
 const Schema = require('../schemas');
 const controller = require('../controllers');
-// const authenticateJWT = require('../middleware/authentication');
 
 loginRoutes.route('/')
   .post(validInputs(Schema.input.loginPOST), controller.login)
-  // .use(authenticateJWT)
   .get(controller.login);
-
-// loginRoutes.route('/:id')
-//  .use(authenticateJWT)
-//   .get()
-//   .put()
-//   .delete();
 
 module.exports = loginRoutes;
