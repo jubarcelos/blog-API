cfcb4521143764f4e7ff7b3713d833c9f0f98b69
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-04-25 15:02:37 -0300

    Update Index contollers
    Incude create and list categories functions in that.

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,14 @@ const createUser = require('./createUser');
 const listUsers = require('./listUsers');
 const login = require('./login');
 const getUser = require('./getUser');
+const createCategory = require('./createCategory');
+const listCategories = require('./listCategories');
 
 module.exports = {
   createUser,
   listUsers,
   login,
   getUser,
+  createCategory,
+  listCategories,
 };
